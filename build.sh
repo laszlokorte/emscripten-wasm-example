d41be6c7ee02dc0271d02a25ad57eaa4ab37f819
Laszlo Korte <email@laszlokorte.de>
2025-12-31 17:49:10 +0100

    add audio

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 emcc main.c \
   -O2 \
   -s WASM=1 \
-  -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' \
+  -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap", "HEAPF32"]' \
   -o main.js
--- main.c
+++ main.c
@@ -1,5 +1,15 @@
+#ifdef __EMSCRIPTEN__
 #include <emscripten/emscripten.h>
+#else
+#define EMSCRIPTEN_KEEPALIVE
+#endif
+
 #include <math.h>
+
+#define SAMPLE_RATE 44100
+#define BLOCK_SIZE 1024
+float block[BLOCK_SIZE];
+float phase = 0.0f;
 
 EMSCRIPTEN_KEEPALIVE
 int map_mouse(int a, int b, int w, int h) {
@@ -7,3 +17,18 @@ int map_mouse(int a, int b, int w, int h) {
   int dy = b - h / 2;
   return (int)(1000 * sqrtl(dx * dx + dy * dy) / sqrtl(w * w + h * h) * 2);
 }
+
+EMSCRIPTEN_KEEPALIVE
+float *generate_block(float freq) {
+  const float step = 2.0f * M_PI * freq / SAMPLE_RATE;
+  for (int i = 0; i < BLOCK_SIZE; i++) {
+    block[i] = sinf(phase);
+    phase += step;
+    if (phase > 2.0f * M_PI)
+      phase -= 2.0f * M_PI;
+  }
+  return block;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int block_size() { return BLOCK_SIZE; }
